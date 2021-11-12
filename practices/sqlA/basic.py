109d64d0355c9509a8080dc6dccdd14dadba1511
ptang39 <ptang39@gatech.edu>
2021-11-11 22:34:19 -0800

    initial final pro

--- practices/sqlA/basic.py
+++ practices/sqlA/basic.py
@@ -1,32 +1,25 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from models import db, Puppy, Owner, Toy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+rufus = Puppy('Rufus')
+fido = Puppy('Fido')
 
-app = Flask(__name__)
+db.session.add_all([rufus, fido])
+db.session.commit()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+print(Puppy.query.all())
 
+rufus = Puppy.query.filter_by(name='Rufus').first()
+print(rufus)
 
-db = SQLAlchemy(app)
-Migrate(app, db)
+jose = Owner('Jose', rufus.id)
 
-class Puppy(db.Model):
-	__tablename__ = 'puppies'
+toy1 = Toy('Chew Toy', rufus.id)
+toy2 = Toy('Ball', rufus.id)
 
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.Text)
-	age = db.Column(db.Integer)
+db.session.add_all([jose, toy1, toy2])
+db.session.comiit()
 
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+rufus = Puppy.query.filter_by(name='Rufus').first()
+print(rufus)
 
-	def __repr__(self):
-		return f"Puppy {self.name} is {self.age} years old "
-
-
-
+rufus.report_toys()
---
+++ practices/sqlA/puppyBlog/app.py
@@ -0,0 +1,12 @@
+from puppycompanyblog import app
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
